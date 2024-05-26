7dfd57a09364592bbfd8588cdf805ba63b2f80c1
yas <yasminpinho199@gmail.com>
2024-05-26 19:08:03 -0400

    Delete ex4.py

--- ex4.py
+++
@@ -1,8 +0,0 @@
-a = "Yasmin"
-b = "Letícia"
-   
-print ("Prezado %a %b. Olá"%(a,b))
-print (10*"victor\n")
-print("+" +10* "-" + "+")
-print (("|" + " " *10 + "|\n")*5, end="")
-print("+" +10* "-" + "+")
