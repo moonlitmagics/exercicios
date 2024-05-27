3d43c5ac1def268d198c706cdc3aef1700469bcf
yas <yasminpinho199@gmail.com>
2024-05-27 09:44:43 -0300

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,7 @@
+texto = str(input("Digite qualquer coisa: "))
+print ("quantidade de caracteres,", len(texto))
+
+texto1 = str(input("Digite um texto: "))
+texto2 = str(input("Digite um texto: "))
+print ("Quantidade de caracteres, ", len(texto1))
+print (len(texto2))
